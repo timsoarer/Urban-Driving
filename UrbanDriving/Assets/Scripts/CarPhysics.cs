b1965176ef41eea7e796f0d7c179e6609ee3b0f8
TimSoarer <timomavr@gmail.com>
2025-01-04 20:03:52 +0500

    Added a template for in-car HUD
    In-car UI in form of a flat HUD to the right of the steering wheel. It contains:
    
    1. A display of the current speed (in km/h), engine rotation (in
    thousands of rotations/min) and gear (parking, reverse, driving, neutral, since it's automatic transmission)
    
    2. A radial menu for switching gears.
    
    Actual functionality is yet to be implemented

--- UrbanDriving/Assets/Scripts/CarPhysics.cs
+++ UrbanDriving/Assets/Scripts/CarPhysics.cs
@@ -57,6 +57,7 @@ public class CarPhysics : MonoBehaviour
     [SerializeField]
     private float accelerationForce = 10f;
 
+
     // Start is called before the first frame update
     void Start()
     {
